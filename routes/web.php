b596dee2abce75177eae4ca9d5dcd5fb067527ac
Kobus Haarhuis <D215294@edu.rocwb.nl>
2017-06-27 13:32:35 +0200

    Dashboard routes setup

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,16 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['middleware' => 'auth'], function () {
+
+  Route::get('/dashboard', 'DashboardController@index');
+
+
+  Route::resource('page', 'PageController');
+  Route::resource('module', 'ModuleController');
+
+});
