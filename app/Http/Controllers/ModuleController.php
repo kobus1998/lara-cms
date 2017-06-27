749518028dcd46dd8b32b23489e042d039608a54
Kobus Haarhuis <D215294@edu.rocwb.nl>
2017-06-27 13:33:47 +0200

    Controller setup

--- app/Http/Controllers/ModuleController.php
+++ app/Http/Controllers/ModuleController.php
@@ -4,9 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class PageController extends Controller
+class ModuleController extends Controller
 {
-
   public function index () {
     // list of all x
   }
@@ -34,5 +33,4 @@ class PageController extends Controller
   public function destroy () {
     // back-end remove
   }
-
 }
