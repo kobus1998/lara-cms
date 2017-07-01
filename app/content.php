b32658354d1ce0f332b7bba9c702d85976efb5ff
Kobus Haarhuis <D215294@edu.rocwb.nl>
2017-07-01 15:20:30 +0200

    relations content

--- app/content.php
+++ app/content.php
@@ -7,10 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class content extends Model
 {
   public function type () {
-    return $this->belongsTo('type');
+    return $this->belongsTo('\App\Type');
   }
 
   public function page () {
     return $this->belongsToMany('\App\Page', 'pages_content', 'content_id', 'page_id');
+  }
+
+  public function module () {
+    return $this->belongsToMany('\App\Module', 'modules_content', 'content_id', 'module_id');
   }
 }
