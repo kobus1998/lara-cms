c16859c5aa147db4bf87b289ccb69f035412eb00
Kobus Haarhuis <D215294@edu.rocwb.nl>
2017-09-25 21:25:44 +0200

    Content and seo pages page

---
+++ resources/views/dashboard/pages/seo.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+
+@section('content')
+
+  <div class="has-margin-bottom">
+    <div class="tabs">
+      <ul>
+        <li class=""><a href="{{ action('PageController@show', $page->id) }}">General</a></li>
+        <li class=""><a href="{{ action('PageController@showContent', $page->id) }}">Content</a></li>
+        <li class="is-active"><a href="{{ action('PageController@showSeo', $page->id) }}">SEO</a></li>
+        <li><a target="_blank" href="{{ action('PageController@route', $page['url']) }}" class="button is-link">Visit</a></li>
+      </ul>
+    </div>
+  </div>
+
+  <div class="page-content">
+    @include('dashboard/pages/forms/update-seo')
+  </div>
+
+
+@endsection
