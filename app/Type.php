827040b01cb516f00c03bc9ee864c4f4e8c33170
Kobus <kobus.haarhuis@hotmail.com>
2017-10-05 09:28:34 +0200

    Clear type function (not neccesairy)

--- app/Type.php
+++ app/Type.php
@@ -6,11 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Type extends Model
 {
-  public function content() {
-    return $this->hasMany('content');
-  }
-
-  // public function page() {
-  //   return $this->hasManyThrough('App\Page');
-  // }
+  
 }
